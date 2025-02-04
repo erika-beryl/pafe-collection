c93211f1f6f234cb26d8abae9bace3f60c2d136a
erika-beryl <haruhaakebono7@gmail.com>
2025-02-04 23:25:43 +0900

    add:db/migrate/20250204132929_create_parfaits.rb,db/schema.rb マイグレーションファイルを書き、マイグレーションを実行

--- db/migrate/20250204132929_create_parfaits.rb
+++ db/migrate/20250204132929_create_parfaits.rb
@@ -1,6 +1,11 @@
 class CreateParfaits < ActiveRecord::Migration[7.1]
   def change
     create_table :parfaits do |t|
+      t.references :shop, null: false, foreign_key: true
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.text :body
+      t.boolean :is_limited, default: false, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_31_135317) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_04_132929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,17 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_31_135317) do
     t.text "business_hours"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "parfaits", force: :cascade do |t|
+    t.bigint "shop_id", null: false
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "body"
+    t.boolean "is_limited", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_parfaits_on_shop_id"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -100,6 +111,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_31_135317) do
   end
 
   add_foreign_key "businesses", "shops"
+  add_foreign_key "parfaits", "shops"
   add_foreign_key "shop_features", "features"
   add_foreign_key "shop_features", "shops"
   add_foreign_key "shop_opentimes", "opentimes"
