1a0bb8e385d5ff837dfc47add9193581e1c47790
erika-beryl <haruhaakebono7@gmail.com>
2025-02-08 23:14:52 +0900

    add:app/controllers/parfaits_controller.rb,app/views/parfaits/_form.html.erb パフェの新規登録のコントローラの記述

--- app/controllers/parfaits_controller.rb
+++ app/controllers/parfaits_controller.rb
@@ -8,6 +8,15 @@ class ParfaitsController < ApplicationController
   end
   
   def create
+    @parfait = Parfait.build(parfait_params)
+
+    if @parfait.save
+      @shop = Shop.find(params[:shop_id])
+      redirect_to shop_path(@shop), success: 'パフェが登録されました'
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Shop.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -17,6 +26,12 @@ class ParfaitsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def parfait_params
+    params.require(:parfait).permit(:name, :body, :price, :is_limited).merge(shop_id: params[:shop_id])
   end
 
 end
