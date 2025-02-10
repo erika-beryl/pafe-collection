f529fb9d6f02d2224e6cbad77ae7702697470100
erika-beryl <haruhaakebono7@gmail.com>
2025-02-10 20:57:55 +0900

    add:app/controllers/parfaits_controller.rb,app/views/parfaits/_top_section_tab.html.erb, app/views/parfaits/edit.html.erb,app/views/parfaits/index.html.erb,app/views/parfaits/show.html.erb,config/routes.rb パフェの詳細画面作成。店舗詳細、パフェ一覧に導線作成

--- app/controllers/parfaits_controller.rb
+++ app/controllers/parfaits_controller.rb
@@ -3,6 +3,10 @@ class ParfaitsController < ApplicationController
     @parfaits = Parfait.includes(:shop).order(created_at: :desc)
   end
 
+  def show
+    load_parfait
+  end
+
   def new
     @parfait = Parfait.new
   end
@@ -11,8 +15,7 @@ class ParfaitsController < ApplicationController
     @parfait = Parfait.build(parfait_params)
 
     if @parfait.save
-      @shop = Shop.find(params[:shop_id])
-      redirect_to shop_path(@shop), success: 'パフェが登録されました'
+      redirect_to shop_path(@parfait.shop), success: 'パフェが登録されました'
     else
       flash.now[:danger] = t('defaults.flash_message.not_created', item: Shop.model_name.human)
       render :new, status: :unprocessable_entity
@@ -26,21 +29,18 @@ class ParfaitsController < ApplicationController
 
   def update
     load_parfait
-    Rails.logger.debug "Params: #{params.inspect}" # 送られてきたパラメータを確認
     if @parfait.update(parfait_params)
       redirect_to shop_path(@parfait.shop), success: 'パフェが更新されました', status: :see_other
     else
-      Rails.logger.debug @parfait.errors.full_messages # ログにエラーを出力
-      flash.now[:danger] = "パフェを更新できませんでした: #{@parfait.errors.full_messages.join(', ')}"
+      flash.now[:danger] = "パフェを更新できませんでした"
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     load_parfait
-    @shop = Shop.find(params[:shop_id])
     @parfait.destroy!
-    redirect_to shops_path(@shop), success: '削除に成功しました'
+    redirect_to shops_path(@parfait.shop), success: '削除に成功しました'
   end
 
   private
