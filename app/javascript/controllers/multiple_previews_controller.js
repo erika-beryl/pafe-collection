a06719cd875c05e68c8bea8fb4cd98d3608e8b60
erika-beryl <haruhaakebono7@gmail.com>
2025-04-01 22:57:30 +0900

    add:   modified:   app/javascript/controllers/multiple_previews_controller.js         modified:   app/views/reviews/_image_form.erb         modified:   app/views/reviews/edit.html.erb reviewの画像投稿欄にプレビュー機能作成

--- app/javascript/controllers/multiple_previews_controller.js
+++ app/javascript/controllers/multiple_previews_controller.js
@@ -4,48 +4,31 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["input", "preview"]
 
-  connect() {
-    const preview = this.previewTarget
-    const width = parseInt(this.data.get('width')) || 400  // デフォルト値 400
-
-    console.log("Controller connected. Width:", width)
-
-    // すでに画像があれば、すべての画像の幅を設定
-    const images = preview.querySelectorAll("img")
-    images.forEach(img => {
-      img.style.maxWidth = `${width}px`
-    })
-  }
-
   previewImage() {
     console.log("previewImage called!")
 
     const input = this.inputTarget
     const preview = this.previewTarget
-    const files = input.files
-    const width = parseInt(this.data.get('width')) || 400
+    console.log("Selected files:", input.files)
 
-    console.log("Selected files:", files)
-
-    // previewエリアをリセット
     preview.innerHTML = ""
 
-    // 複数ファイルが選択されていれば、それぞれに対してプレビューを生成
-    if (files && files.length) {
-      Array.from(files).forEach(file => {
-        const reader = new FileReader()
-        reader.onload = (e) => {
-          console.log("File loaded:", e.target.result)  // 確認用
+    if (input.files.length === 0) return;
 
-          const img = document.createElement("img")
-          img.src = e.target.result
-          img.style.maxWidth = `${width}px`
-          preview.appendChild(img)
-        }
-        reader.readAsDataURL(file)
-      })
-    }
+    Array.from(input.files).forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const img = document.createElement("img");
+        img.src = e.target.result;
+        img.style.maxWidth = "200px";
+        img.style.marginRight = "5px";
+        preview.appendChild(img);
+      };
+
+      reader.readAsDataURL(file);
+    });
+
+    this.previewTarget.parentElement.style.display = "";
   }
 }
-
-
