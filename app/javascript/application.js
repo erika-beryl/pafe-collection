00d88750bd981d8227688cd737ddfb3d9b187fbd
erika-beryl <haruhaakebono7@gmail.com>
2025-04-06 00:28:56 +0900

    add:スライドショー実装未達成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,22 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import $ from "jquery";
+import "slick-carousel";  // slickをインポート
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"; 
 import * as bootstrap from "bootstrap"
+
+window.$ = window.jQuery = $;
+window.bootstrap = bootstrap
+
+document.addEventListener("turbo:load", function () {
+  $(".slider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    slidesToShow: 1,
+    centerMode: true
+  });
+});
