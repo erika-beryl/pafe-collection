eee11850774cf6f01dd11e245334603b00fa6f75
erika-beryl <haruhaakebono7@gmail.com>
2025-04-20 21:09:25 +0900

    add:parfaitでもリサイズしてから保存できるようにした  modified:   app/controllers/parfaits_controller.rb

--- app/controllers/parfaits_controller.rb
+++ app/controllers/parfaits_controller.rb
@@ -20,6 +20,18 @@ class ParfaitsController < ApplicationController
   def create
     @parfait = Parfait.new(parfait_params)
 
+    if params[:parfait][:parfait_image].present?
+      resized_images = resize_image_set_dpi(params[:parfait][:parfait_image])
+      original_filename_base = File.basename(params[:parfait][:parfait_image].original_filename, ".*")
+
+      @parfait.parfait_image.attach(
+        io: resized_images,
+        filename: "#{original_filename_base}.jpg",
+        # ここでcontent_typeを指定しないと名前は.jpgの拡張子が付いているけどデータはpngみたいなことが起こる可能性がある
+        content_type: 'image/jpg'
+      )
+    end
+
     if @parfait.save
       redirect_to @parfait, success: 'パフェが登録されました'
     else
@@ -36,9 +48,20 @@ class ParfaitsController < ApplicationController
   def update
     load_parfait
 
-    if parfait_params[:remove_parfait_image] == '1'
+    if params[:parfait][:remove_parfait_image] == '1'
       @parfait.parfait_image.purge if @parfait.parfait_image.attached?
-      @parfait.parfait_image = nil
+    end
+
+    if params[:parfait][:parfait_image].present?
+      resized_images = resize_image_set_dpi(params[:parfait][:parfait_image])
+      original_filename_base = File.basename(params[:parfait][:parfait_image].original_filename, ".*")
+
+      @parfait.parfait_image.attach(
+        io: resized_images,
+        filename: "#{original_filename_base}.jpg",
+        # ここでcontent_typeを指定しないと名前は.jpgの拡張子が付いているけどデータはpngみたいなことが起こる可能性がある
+        content_type: 'image/jpg'
+      )
     end
 
     if @parfait.update(parfait_params.except(:remove_parfait_image))
@@ -58,11 +81,29 @@ class ParfaitsController < ApplicationController
   private
 
   def parfait_params
-    params.require(:parfait).permit(:name, :body, :price, :is_limited, :parfait_image, :remove_parfait_image).merge(shop_id: params[:shop_id] || @parfait&.shop_id)
+    params.require(:parfait).permit(:name, :body, :price, :is_limited).merge(shop_id: params[:shop_id] || @parfait&.shop_id)
   end
 
   def load_parfait
     @parfait = Parfait.includes(:shop).find(params[:id])
+  end
+
+  def resize_image_set_dpi(uploaded_file)
+    # uploaded_file.tempfileから画像を読み込み、MiniMagickオブジェクトとして扱う
+    image = MiniMagick::Image.read(uploaded_file.tempfile)
+    # 画像の縦幅を1350ピクセルにリサイズ
+    image.resize 'x1350'
+    # 画像の解像度を96 DPIに設定
+    image.density '96'
+
+    # 一時ファイルを作成
+    tempfile_jpg = Tempfile.new('resized')
+    # 変更された画像を一時ファイルに書き込み（ここで画像が指定されたリサイズと解像度で保存される）
+    image.write (tempfile_jpg.path)
+    # ファイルを読み込む準備
+    tempfile_jpg.rewind
+    # 一時ファイルを呼び出す
+    tempfile_jpg
   end
 
 end
