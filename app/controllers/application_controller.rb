3dc110c7e8e3877478ca6a5b1ac2f830ca7aa8ef
erika-beryl <haruhaakebono7@gmail.com>
2025-05-06 22:31:29 +0900

    add:プロフィールにアバター機能追加 modified:   app/controllers/application_controller.rb         modified:   app/models/review.rb         modified:   app/models/user.rb         modified:   app/views/devise/registrations/edit.html.erb         modified:   app/views/shared/_header.html.erb         modified:   app/views/users/show.html.erb         modified:   config/locales/devise.ja.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :avatar])
   end
 end
