f44e3d948e31f59aafbe85333382b053b2366cc9
erika-beryl <haruhaakebono7@gmail.com>
2025-05-06 22:02:16 +0900

    add:プロフィール画面と編集画面実装  modified:   app/controllers/users/registrations_controller.rb         new file:   app/controllers/users_controller.rb         new file:   app/helpers/users_helper.rb         modified:   app/views/devise/registrations/edit.html.erb         modified:   app/views/shared/_header.html.erb         new file:   app/views/users/show.html.erb         renamed:    config/locales/devise.en.yml -> config/locales/devise.ja.yml         modified:   config/locales/ja.yml         modified:   config/routes.rb         new file:   test/controllers/users_controller_test.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  protected
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def after_update_path_for(resource)
+    users_profile_path
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
