f1b08d8cb4157bbba664848f62ee949986414a8d
erika-beryl <haruhaakebono7@gmail.com>
2025-06-21 20:02:45 +0900

    add:passwordユーザーがこのままではuidのバリデーションエラーでログインできないので対策をした。 app/models/user.rb db/migrate/20250621100959_add_uid_provider_index_to_users.rb db/schema.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
+
+  before_create :set_provider_uid_for_email_user
   
   has_many :reviews, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
@@ -23,11 +25,19 @@ class User < ApplicationRecord
     id == object&.user_id
   end
 
+  def set_provider_uid_for_email_user
+    if provider.blank? && uid.blank?
+      Rails.logger.info "=== set_provider_uid_for_email_user called ==="
+      self.provider = 'default'
+      self.uid = SecureRandom.uuid
+    end
+  end
+    
   def self.from_omniauth(auth)
     user = User.find_by(email: auth.info.email)
 
     if user
-      user.update(provider: auth.provider, uid: auth.uid) unless user.provider.present? && user.uid.present?
+      user.update(provider: auth.provider, uid: auth.uid)
       return user
     else
       User.create!(
