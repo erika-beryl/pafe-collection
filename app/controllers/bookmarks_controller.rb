58a87efc2ba43d14370c9cfc56a21a3b1111c262
erika-beryl <haruhaakebono7@gmail.com>
2025-06-22 19:34:53 +0900

    add:bookmark機能をajax化 app/views/shared/_unbookmark.html.erb app/views/shared/_bookmark_buttons.html.erb app/views/reviews/_review.html.erb app/views/bookmarks/destroy.turbo_stream.erb app/views/bookmarks/create.turbo_stream.erb app/controllers/bookmarks_controller.rb

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,13 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
   def create
-    review = Review.find(params[:review_id])
-    current_user.bookmark(review)
-    redirect_back fallback_location: root_path, success: t('.success')
+    @review = Review.find(params[:review_id])
+    current_user.bookmark(@review)
   end
 
   def destroy
-    bookmark = current_user.bookmarks.find(params[:id])
-    bookmark.destroy
-    redirect_back fallback_location: root_path, success: t('.success')
+    @review = current_user.bookmarks.find(params[:id]).review
+    current_user.unbookmark(@review)
   end
 
 end
