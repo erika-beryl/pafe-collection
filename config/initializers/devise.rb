5978feec124ca00c5042dc10ce3c886b16a5750e
erika-beryl <haruhaakebono7@gmail.com>
2025-06-25 15:55:30 +0900

    add:rspecテスト時google連携は使わないのでconfig/initializers/devise.rbの設定でテスト環境を省くようにする config/initializers/devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,11 +273,14 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :google_oauth2, 
-                 ENV['GOOGLE_CLIENT_ID'].presence || Rails.application.credentials.google[:google_client_id],
-                 ENV['GOOGLE_CLIENT_SECRET'].presence || Rails.application.credentials.google[:google_client_secret], {
-                  scope: 'userinfo.email, userinfo.profile'
-                 }
+  unless Rails.env.test?
+    config.omniauth :google_oauth2, 
+                  ENV['GOOGLE_CLIENT_ID'].presence || Rails.application.credentials.dig(:google, :google_client_id),
+                  ENV['GOOGLE_CLIENT_SECRET'].presence || Rails.application.credentials.dig(:google, :google_client_secret), {
+                    scope: 'userinfo.email, userinfo.profile'
+                  }
+  end
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
