c257215ef3906df2c2a32119667520c8d4dd9e56
erika-beryl <haruhaakebono7@gmail.com>
2025-06-26 21:33:35 +0900

    add:userのテストケースを記載 spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー登録' do
+    it 'name, email, passwordとpassword_confirmationが存在すれば登録できる' do end
+    it 'emailがない場合にinvalidになるか' do end
+    it 'nameが無い場合にinvalidになるか' do end
+    it 'passwordが無い場合にinvalidになるか' do end
+    it 'password_confirmationが無い場合にinvalidになるか' do end
+    it 'passwordとpassword_confirmationが一致しないとinvalidになるか' do end     
 end
