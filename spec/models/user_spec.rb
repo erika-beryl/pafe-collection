7e5b40892aef8dc3b8b0374d4d1963049312e694
erika-beryl <haruhaakebono7@gmail.com>
2025-06-29 13:28:57 +0900

    add:factorybotのuser作成 spec/factories/user.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー登録' do
-    it 'name, email, passwordとpassword_confirmationが存在すれば登録できる' do end
+    it 'name, email, passwordとpassword_confirmationが存在すれば登録できる' do
+    
+    end
     it 'emailがない場合にinvalidになるか' do end
     it 'nameが無い場合にinvalidになるか' do end
     it 'passwordが無い場合にinvalidになるか' do end
     it 'password_confirmationが無い場合にinvalidになるか' do end
-    it 'passwordとpassword_confirmationが一致しないとinvalidになるか' do end     
+    it 'passwordとpassword_confirmationが一致しないとinvalidになるか' do end
+    it '同じメールアドレスは登録できない'      
 end
